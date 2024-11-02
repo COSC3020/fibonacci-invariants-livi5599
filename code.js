91a3091d92607229cb5dea46ea3c8f85ba7c2049
livi5599 <obrown1@uwyo.edu>
2024-11-02 16:12:52 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,15 +1,16 @@
 function fib(n) {
     let a = Array(n+1);
 
+    a[0] = 0;
+
     fibHelper(n, a);
 
     return a;
 }
 
 function fibHelper(n, arr) {
-    if (n == 0) {
-        arr[0] = 0;
-        return 0; 
+    if (n < 0) {
+        return arr;
     }
 
     if (n == 1) {
@@ -21,6 +22,6 @@ function fibHelper(n, arr) {
         return arr[n];
     }
 
-    arr[n] = fibHelper(n-1, arr) + fibHelper(n-2, arr);
+    arr[n] = fibHelper(n-2, arr) + fibHelper(n-1, arr);
     return arr[n];
 }
